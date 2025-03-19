9c00a260d8978e55be672af342bd08aae8394d34
Im60duck <kvashnin.andruha@yandex.ru>
2025-03-19 19:45:31 +0700

    Routing

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,12 @@
 import Main from '../../pages/main/main';
+import Favorites from '../../pages/favorites/favorites';
+import Offer from '../../pages/offer/offer';
+import Login from '../../pages/login/login';
+import NotFound from '../../pages/notFound/notFound';
+import { AppRoute, AuthorizationStatus } from '../../const.ts';
+import PrivateRoute from '../private-route/private-route.tsx';
+import {Route, BrowserRouter, Routes} from 'react-router-dom';
+
 
 type AppProps = {
   placesToStay: number;
@@ -6,7 +14,34 @@ type AppProps = {
 
 function App({placesToStay}: AppProps): JSX.Element {
   return (
-    <Main placesToStay = {placesToStay}/>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Main}
+          element = {<Main placesToStay = {placesToStay}/>}
+        />
+        <Route
+          path={AppRoute.Favorites}
+          element = {
+            <PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth}>
+              <Favorites />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path={AppRoute.Offer}
+          element = {<Offer/>}
+        />
+        <Route
+          path={AppRoute.Login}
+          element = {<Login/>}
+        />
+        <Route
+          path='*'
+          element = {<NotFound/>}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/notFound/notFound.tsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+import { AppRoute } from '../../const';
+
+function NotFound(): JSX.Element {
+  return(
+    <>
+      <p>404 Not Found</p>
+      <Link className="back_to_main" to={AppRoute.Main}>Back to main page</Link>
+    </>
+  );
+}
+
+export default NotFound;
